f46bd0919e8440b56786d29363d1b42eaaa1fa4a
Aero <ooostarinnightooo@gmail.com>
2016-01-16 12:45:05 +0700

    Change database

--- BuyLaptop/Resources.cs
+++ BuyLaptop/Resources.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.Windows.Forms;
 
 namespace BuyLaptop
 {
@@ -22,8 +23,8 @@ namespace BuyLaptop
 		    };
             public static string[] Type =
 		    {
+			    "prices",
 			    "fashion",
-			    "price",
 			    "specs",
 		    };
           
@@ -31,48 +32,57 @@ namespace BuyLaptop
         }
 		public Resources()
 		{
-			m_database = new SQLiteDatabase("laptop.s3db");
 
-			//Insert();
+            if (System.IO.File.Exists("laptop.s3db"))
+                m_database = new SQLiteDatabase("laptop.s3db");
+            else
+            {
+                OpenFileDialog op = new OpenFileDialog();
+                op.Filter = "Database File|*.s3db";
+                op.ShowDialog();
+                String FilePath = op.FileName;
+                m_database = new SQLiteDatabase(FilePath);
+            }
+			Insert();
 		}
 
 		private void Insert()
 		{
             m_database.ClearTable("product");
-            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 3543", "500GB", "i3", "No", "15.6", "4GB", "fashion","9 999 000"));
-            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 7348T", "500GB", "i5", "No", "13.3", "8GB", "fashion", "18 999 000"));
-            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 3458", "500GB", "i3", "820M 2GB", "14", "4GB", "specs", "9 999 000"));
-            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 7559", "1000GB", "i5", "GTX960M 4GB", " 15.6", "specs", "18 399 000", ""));
-            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 3458-TXTGH2", "500GB", "i3", "No", "14", "4GB", "price", "9 899 000"));
-            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 5459", "1000GB", "i7", "Radeon R5 M335 4GB", "15.6", "8GB", "price", "18 999 000"));
+            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 3543", "500GB", "i3", "No", "15.6", "4GB", "fashion","9999000"));
+            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 7348T", "500GB", "i5", "No", "13.3", "8GB", "fashion", "18999000"));
+            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 3458", "500GB", "i3", "820M 2GB", "14", "4GB", "specs", "9999000"));
+            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 7559", "1000GB", "i5", "GTX960M 4GB", " 15.6", "specs", "18399000", ""));
+            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 3458-TXTGH2", "500GB", "i3", "No", "14", "4GB", "prices", "9899000"));
+            m_database.Insert("product", CreateDictionary("DELL", "INSPIRON 5459", "1000GB", "i7", "Radeon R5 M335 4GB", "15.6", "8GB", "prices", "18999000"));
 
-            m_database.Insert("product", CreateDictionary("HP", "PAVILION x360", "500GB", "PENTIUM", "No", "4GB", "11", "fashion", "9 999 000"));
-            m_database.Insert("product", CreateDictionary("HP", "PAVILION 15 ", "1000GB", "i7", "GT940M 2GB", "8GB", "15", "fashion", "16 899 000"));
+            m_database.Insert("product", CreateDictionary("HP", "PAVILION x360", "500GB", "PENTIUM", "No", "4GB", "11", "fashion", "9999000"));
+            m_database.Insert("product", CreateDictionary("HP", "PAVILION 15 ", "1000GB", "i7", "GT940M 2GB", "8GB", "15", "fashion", "16899000"));
             m_database.Insert("product", CreateDictionary("HP", "350 G2", "500GB", "", "i3", "No", "14", "specs", "9 299 000"));
-            m_database.Insert("product", CreateDictionary("HP", "PAVILION 15 ", "1000GB", "i7", "GT940M 2GB", "4GB", "15", "specs", "17 899 000"));
-            m_database.Insert("product", CreateDictionary("HP", "PAVILION 14", "500GB", "i3", "No", "4GB", "14", "price", "9 499 000"));
-            m_database.Insert("product", CreateDictionary("HP", "PAVILION 15", "1000GB", "i7", "GT840M 2GB", "4GB", "15", "price", "15 399 000"));
+            m_database.Insert("product", CreateDictionary("HP", "PAVILION 15 ", "1000GB", "i7", "GT940M 2GB", "4GB", "15", "specs", "17899000"));
+            m_database.Insert("product", CreateDictionary("HP", "PAVILION 14", "500GB", "i3", "No", "4GB", "14", "prices", "9499000"));
+            m_database.Insert("product", CreateDictionary("HP", "PAVILION 15", "1000GB", "i7", "GT840M 2GB", "4GB", "15", "prices", "15399000"));
 
-            m_database.Insert("product", CreateDictionary("LENOVO", "S410", "500GB", "i3", "No", "14", "4GB", "fashion", "9 390 000"));
-            m_database.Insert("product", CreateDictionary("LENOVO", "Z5070", "1000GB", "i7", "GT 840M 4GB", "15.6", "4GB", "fashion", "16 499 000"));
-            m_database.Insert("product", CreateDictionary("LENOVO", "S410", "500GB", "i3", "No", "14", "4GB", "specs", "9 399 000"));
-            m_database.Insert("product", CreateDictionary("LENOVO", "IDEAPAD", "1000GB", "i7", "R7 M360", "14", "4GB", "specs", "16 999 000"));
-            m_database.Insert("product", CreateDictionary("LENOVO", "S410", "500GB", "i3", "No", "14", "4GB", "price", "9 399 000"));
-            m_database.Insert("product", CreateDictionary("LENOVO", "Thinkpad E560", "500GB", "i5", "R7 M330 2GB", "15.6", "4GB", "price", "14 999 000"));
+            m_database.Insert("product", CreateDictionary("LENOVO", "S410", "500GB", "i3", "No", "14", "4GB", "fashion", "9390000"));
+            m_database.Insert("product", CreateDictionary("LENOVO", "Z5070", "1000GB", "i7", "GT 840M 4GB", "15.6", "4GB", "fashion", "16499000"));
+            m_database.Insert("product", CreateDictionary("LENOVO", "S410", "500GB", "i3", "No", "14", "4GB", "specs", "9399000"));
+            m_database.Insert("product", CreateDictionary("LENOVO", "IDEAPAD", "1000GB", "i7", "R7 M360", "14", "4GB", "specs", "16999000"));
+            m_database.Insert("product", CreateDictionary("LENOVO", "S410", "500GB", "i3", "No", "14", "4GB", "prices", "9399000"));
+            m_database.Insert("product", CreateDictionary("LENOVO", "Thinkpad E560", "500GB", "i5", "R7 M330 2GB", "15.6", "4GB", "prices", "14999000"));
 
             m_database.Insert("product", CreateDictionary("ASUS", "F200MA", "500GB", "PENTIUM", "No", "11.6", "4GB", "fashion", "8599000"));
             m_database.Insert("product", CreateDictionary("ASUS", "K501LX", "1000GB", "i5", "GTX950M 4GB", "15.6", "8GB", "fashion", "15899000"));
             m_database.Insert("product", CreateDictionary("ASUS", "K455LD", "500GB", "i3", "GT820M 2GB", "14", "8GB", "specs", "9899000"));
             m_database.Insert("product", CreateDictionary("ASUS", "K501LX", "500GB", "i7", "GTX950M 4GB", "15.6", "8GB", "specs", "19299000"));
-            m_database.Insert("product", CreateDictionary("ASUS", "X554LD", "500GB", "i3", "GT820M 1GB", "15.6", "4GB", "price", "9599000"));
-            m_database.Insert("product", CreateDictionary("ASUS", "K501LX", "500GB", "i7", "GTX 950M 4GB", "15.6", "4GB", "price", "17350000"));
+            m_database.Insert("product", CreateDictionary("ASUS", "X554LD", "500GB", "i3", "GT820M 1GB", "15.6", "4GB", "prices", "9599000"));
+            m_database.Insert("product", CreateDictionary("ASUS", "K501LX", "500GB", "i7", "GTX 950M 4GB", "15.6", "4GB", "prices", "17350000"));
 
-            m_database.Insert("product", CreateDictionary("ACER", "Z1402 350L", "500GB", "i3", "No", "14", "4GB", "fashion", "8 299 000"));
-            m_database.Insert("product", CreateDictionary("ACER", "E5-574G-59DA", "500GB", "i5", "GT920M 2GB", "15.6", "4GB", "fashion", "12 699 000"));
-            m_database.Insert("product", CreateDictionary("ACER", "F5-571-55EJ", "500GB", "i5", "No", "15.6", "4GB", "specs", "9 999 000"));
-            m_database.Insert("product", CreateDictionary("ACER", "V3-371-77Z0", "240SSD", "i7", "No", "13.3", "4GB", "specs", "17 499 000"));
-            m_database.Insert("product", CreateDictionary("ACER", "E5-473-58HC", "500GB", "i5", "No", "14", "4B", "price", "9 390 000"));
-            m_database.Insert("product", CreateDictionary("ACER", "V5-591G-51J7", "1000GB", "i5", "GTX950M 2GB", "15.6", "4GB", "price", "15 299 000"));
+            m_database.Insert("product", CreateDictionary("ACER", "Z1402 350L", "500GB", "i3", "No", "14", "4GB", "fashion", "8299000"));
+            m_database.Insert("product", CreateDictionary("ACER", "E5-574G-59DA", "500GB", "i5", "GT920M 2GB", "15.6", "4GB", "fashion", "12699000"));
+            m_database.Insert("product", CreateDictionary("ACER", "F5-571-55EJ", "500GB", "i5", "No", "15.6", "4GB", "specs", "9999000"));
+            m_database.Insert("product", CreateDictionary("ACER", "V3-371-77Z0", "240SSD", "i7", "No", "13.3", "4GB", "specs", "17499000"));
+            m_database.Insert("product", CreateDictionary("ACER", "E5-473-58HC", "500GB", "i5", "No", "14", "4B", "prices", "9390000"));
+            m_database.Insert("product", CreateDictionary("ACER", "V5-591G-51J7", "1000GB", "i5", "GTX950M 2GB", "15.6", "4GB", "prices", "15299000"));
 
 		}
         //public Dictionary<string, string> CreateDictionary(string id, string brand, string name, string code, string cpu, string ram, string price)
