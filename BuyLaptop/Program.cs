f83d0db6f1c2567ba85df21949fc40b1b9bb670b
Aero <ooostarinnightooo@gmail.com>
2016-01-16 12:43:47 +0700

    Handle Prolog is not installed

--- BuyLaptop/Program.cs
+++ BuyLaptop/Program.cs
@@ -15,22 +15,22 @@ namespace BuyLaptop
         [STAThread]
         static void Main()
         {
-			if(System.IO.Directory.Exists("C:\\Program Files (x86)\\swipl\\"))
-			{
-				Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\Program Files (x86)\swipl");
-			}
-			else if(System.IO.Directory.Exists("C:\\Program Files\\swipl\\"))
-			{
-				Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\Program Files\swipl");
-			}
-            else
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (!System.IO.Directory.Exists("C:\\Program Files (x86)\\swipl\\") && !System.IO.Directory.Exists("C:\\Program Files\\swipl\\"))
             {
-                MessageBox.Show("Prolog chưa được cài đăt hoặc vị trí sai, vui lòng cài đặt Prolog 6.6.5 vào vị trí mặc định trên ổ C.");
-                return;
+                MessageBox.Show("Prolog chưa được cài đăt hoặc vị trí sai, vui lòng cài đặt Prolog 6.6.5 vào vị trí mặc định trên ổ C và chọn lại đường dẫn đến thư mục swipl cài đặt Prolog, nếu không chương trình không thể chạy!");
+                FolderBrowserDialog path = new FolderBrowserDialog();
+                path.ShowDialog();
+                String FolderPath = path.SelectedPath;
+                Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"" + FolderPath);
             }
+            else if (System.IO.Directory.Exists("C:\\Program Files (x86)\\swipl\\"))
+                Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\Program Files (x86)\swipl");
+            else if (System.IO.Directory.Exists("C:\\Program Files\\swipl\\"))
+                Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\Program Files\swipl");
 
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
+			
 			if (!PlEngine.IsInitialized)
             {
                 String[] empty_param = { "" };
